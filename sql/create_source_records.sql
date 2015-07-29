819c7596f1da5275a9000254512cd678189db52f
Joshua Steverman <jstever@punch.umdl.umich.edu>
2015-07-29 12:07:12 -0400

    removing errant gd_id column from gd_source_recs table

--- sql/create_source_records.sql
+++ sql/create_source_records.sql
@@ -1,19 +1,19 @@
 use ht_repository;
-drop table if exists gd_source_recs;
+/*drop table if exists gd_source_recs;
 
 CREATE TABLE gd_source_recs (
   file_path VARCHAR(255) NOT NULL,
   line_number INT NOT NULL,
   file_input_id INT null,
-  gd_id INT null,
   source MEDIUMTEXT NOT NULL
 );
+*/
+/* after adding records */
 
-/* after adding records 
+CREATE INDEX file_line ON gd_source_recs (file_path, line_number);
 
-CREATE INDEX file_line (file_path, line_number) ON gd_source_recs;
-
-/* this won't get all of them, fix that tar.gz stuff manually
+/* this won't get all of them, fix that tar.gz stuff manually */
 UPDATE gd_source_recs s, hathi_input_file f SET s.file_input_id = f.id
   WHERE s.file_path = f.file_path;
 
+CREATE INDEX fid_line ON gd_source_recs (file_input_id, line_number);
