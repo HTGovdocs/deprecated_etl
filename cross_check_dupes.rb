a8bf1f6b6b7c65b6396f48ec52f06adeadeaf58d
Joshua Steverman <jstever@punch.umdl.umich.edu>
2015-09-04 13:47:23 -0400

    filtering out dupe clusters with oclc nums

---
+++ cross_check_dupes.rb
@@ -0,0 +1,18 @@
+#are there 1.0 duplicate clusters with govdocs missing oclc numbers?
+require 'htph'
+@jdbc = HTPH::Hathijdbc::Jdbc.new();
+@conn = @jdbc.get_conn();
+@get_oclc = "SELECT count(*) as c from hathi_oclc where gd_id IN(?)"
+cluster_gd_ids = open(ARGV.shift)
+
+
+
+cluster_gd_ids.each do | line |
+  line.chomp!
+  @conn.prepared_select(@get_oclc, [line.split(/\t/)[2]]) do | row |
+    count = row.get_object('c')
+    if count.to_i == 0
+      puts line
+    end
+  end
+end
